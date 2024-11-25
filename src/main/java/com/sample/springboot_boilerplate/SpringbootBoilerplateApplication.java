2e3f6f3ae194804d8f999e23b85dac019f777dcf
Prem Dharshan <whizzkid.dpd@gmail.com>
2024-11-25 11:49:19 +0530

    Add Hello world to console

--- src/main/java/com/sample/springboot_boilerplate/SpringbootBoilerplateApplication.java
+++ src/main/java/com/sample/springboot_boilerplate/SpringbootBoilerplateApplication.java
@@ -7,7 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootBoilerplateApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringbootBoilerplateApplication.class, args);
+
+        // Let's get started by simply printing "Hello World" to the console
+        System.out.println("\n\n\nHello World\n\n\n");
+
+        // Once done, run the application using the following command:
+        // ./gradlew build
+        // ./gradlew bootRun
+
+        // Check the terminal for the output
+        // Once done checkout or switch to helloworld branch
     }
 
 }
